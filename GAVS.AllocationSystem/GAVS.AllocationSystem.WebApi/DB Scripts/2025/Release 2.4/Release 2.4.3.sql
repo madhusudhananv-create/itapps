108b0f252cb6b53f7a902033e6727a860acedae3
srividhya.b <srividhya.b@70b15045-743b-414c-a502-22bcac4871f6>
2025-05-05 06:55:47 +0000

    130003616 — Need to merge 2 Audit check list for a Audit
    git-svn-id: http://svn.gavstech.com/RAS-BAS/CSP/SourceCode/Main@6198 70b15045-743b-414c-a502-22bcac4871f6

--- GAVS.AllocationSystem/GAVS.AllocationSystem.WebApi/DB Scripts/2025/Release 2.4/Release 2.4.3.sql
+++ GAVS.AllocationSystem/GAVS.AllocationSystem.WebApi/DB Scripts/2025/Release 2.4/Release 2.4.3.sql
@@ -68,3 +68,59 @@ INSERT INTO configuration_ext (
 );
 END
 GO
+
+Declare  @RESOURCEID int = 825
+
+Declare @RescourceName varchar(250) = 'SQA Management > Merge Checklist'
+if not exists(select 1 from csp..APP_CONTROLS where RESOURCE_NAME = @RescourceName) 
+begin
+
+	insert into csp..APP_CONTROLS (RESOURCE_ID,RESOURCE_TYPE,RESOURCE_NAME,COMMENTS,CREATED_BY,UPDATED_BY) 
+	values (@RESOURCEID,'Control',@RescourceName,null,104744,104744)
+
+	set @RESOURCEID = (select RESOURCE_ID from csp..APP_CONTROLS where RESOURCE_NAME = @RescourceName )
+
+end
+
+if not exists(select 1 from csp..APP_ACCESS_CONTROLS where RESOURCE_ID = @RESOURCEID)
+begin
+
+	insert into csp..APP_ACCESS_CONTROLS 
+	 (RESOURCE_ID,ROLE_ID,EMP_ID,CUST_ID,PROJ_ID,COMMENTS,CREATED_BY,UPDATED_BY,VIEW_ACCESS,CREATE_ACCESS,
+	 EDIT_ACCESS,DELETE_ACCESS,DEFAULT_ACCESS) 
+	 values (@RESOURCEID,1,'','','',null,104744,104744,1,0,0,0,0),
+	 (@RESOURCEID,2,'','','',null,104744,104744,1,0,0,0,0),
+	 (@RESOURCEID,3,'','','',null,104744,104744,0,0,0,0,0),
+	 (@RESOURCEID,4,'','','',null,104744,104744,0,0,0,0,0),
+	 (@RESOURCEID,5,'','','',null,104744,104744,0,0,0,0,0),
+	 (@RESOURCEID,6,'','','',null,104744,104744,0,0,0,0,0),
+	 (@RESOURCEID,7,'','','',null,104744,104744,1,1,1,1,1),
+	 (@RESOURCEID,8,'','','',null,104744,104744,0,0,0,0,0),
+	 (@RESOURCEID,9,'','','',null,104744,104744,0,0,0,0,0),
+	 (@RESOURCEID,10,'','','',null,104744,104744,0,0,0,0,0),
+	 (@RESOURCEID,11,'','','',null,104744,104744,0,0,0,0,0),
+	 (@RESOURCEID,12,'','','',null,104744,104744,0,0,0,0,0),
+     (@RESOURCEID,13,'','','',null,104744,104744,0,0,0,0,0)
+
+
+end
+
+if not exists (select 1 from csp..APP_CONTROL_FEATURES where RESOURCE_ID = @RESOURCEID)
+begin
+
+	insert into csp..APP_CONTROL_FEATURES (RESOURCE_ID,FEATURE,COMMENTS,CREATED_BY,UPDATED_BY) 
+	values (@RESOURCEID,'VIEW',null,104744,104744),
+	(@RESOURCEID,'CREATE',null,104744,104744),
+	(@RESOURCEID,'EDIT',null,104744,104744),
+	(@RESOURCEID,'DELETE',null,104744,104744)
+	
+
+end
+GO
+
+IF NOT EXISTS(Select * from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME = 'PM_CHECKLIST' AND COLUMN_NAME='IS_MERGED')
+BEGIN
+ALTER TABLE PM_CHECKLIST add IS_MERGED BIT;
+END
+GO
+
