5d0603d80ca4240f6fab36bfe51126c6dabe9cdc
srividhya.b <srividhya.b@70b15045-743b-414c-a502-22bcac4871f6>
2025-05-20 07:07:40 +0000

    Merge checklist changes
    git-svn-id: http://svn.gavstech.com/RAS-BAS/CSP/SourceCode/Main@6220 70b15045-743b-414c-a502-22bcac4871f6

--- csp-app/src/app/models/checklist-model.ts
+++ csp-app/src/app/models/checklist-model.ts
@@ -20,7 +20,7 @@ export class ChecklistModel {
     findingstypE_ID: number;
     updateD_NAME: string;
     findingtypE_VALUE: string;
-    IS_MERGED: boolean = true;
+    iS_MERGED: boolean = false;
 }
 
 export class ChecklistQuestionsModelNew {
