ea0f84a3a3a6f0a14d11343dc9aac4091768c5ad
srividhya.b <srividhya.b@70b15045-743b-414c-a502-22bcac4871f6>
2025-09-16 08:36:57 +0000

    ACSAT Script changes added
    git-svn-id: http://svn.gavstech.com/RAS-BAS/CSP/SourceCode/Main@6437 70b15045-743b-414c-a502-22bcac4871f6

--- GAVS.AllocationSystem/GAVS.AllocationSystem.WebApi/DB Scripts/2025/Release 2.4/Release 2.4.7.sql
+++ GAVS.AllocationSystem/GAVS.AllocationSystem.WebApi/DB Scripts/2025/Release 2.4/Release 2.4.7.sql
@@ -1,22 +1,76 @@
 ﻿
-----From 04/09/2025----
- if Exists(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='css_batch_customers' and COLUMN_NAME='cust_id' and(DATA_TYPE <> 'varchar(50)'))
+----ACSAT SCRIPTS----
+
+
+ IF Exists(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='css_batch_customers' and COLUMN_NAME='cust_id' and(DATA_TYPE <> 'varchar(50)'))
 BEGIN
  alter table css_batch_customers alter column cust_id varchar(50) not null
 END
-if Exists(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='css_batch_customers' and COLUMN_NAME='proj_id' and(DATA_TYPE <> 'varchar(50)'))
+
+GO
+
+IF Exists(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='css_batch_customers' and COLUMN_NAME='proj_id' and(DATA_TYPE <> 'varchar(50)'))
 BEGIN
  alter table css_batch_customers alter column proj_id varchar(50)   null
 END
-if Exists(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='customer_projects' and COLUMN_NAME='proj_id' and(DATA_TYPE <> 'varchar(50)'))
+
+GO
+
+IF Exists(select 1 from INFORMATION_SCHEMA.COLUMNS where TABLE_NAME='customer_projects' and COLUMN_NAME='proj_id' and(DATA_TYPE <> 'varchar(50)'))
 BEGIN
     alter table customer_projects alter column proj_id varchar(50) null
 END
 
-If  exists (select 1 from configuration_ext WHERE [KEY]='CSS_QUESTION_MODEL_HALFYEARLY_ACCOUNT')   
+GO
+
+IF NOT exists (select 1 from configuration_ext WHERE [KEY]='CSS_QUESTION_MODEL_HALFYEARLY_ACCOUNT')   
 BEGIN
 insert into configuration_ext values
 ('CSS_QUESTION_MODEL_HALFYEARLY_ACCOUNT', 12, -1, null, null, 0, 1, null, null, null, '102802', getdate(), '102802', getdate())
 END
 
-update task set STATUS = 'COMPLETED' where ID=4807
+GO
+
+ IF NOT EXISTS (SELECT 1 FROM CSS_QUESTION_MODELS WHERE MODEL_NAME ='ACSAT')   
+ BEGIN
+
+INSERT INTO CSS_QUESTION_MODELS VALUES ('ACSAT','CSAT Related questions','104744',getdate(),'104744',getdate(),1,'Account')
+
+END
+
+GO
+
+DECLARE @MODELID INT
+
+SET @MODELID = (SELECT ID FROM CSS_QUESTION_MODELS WHERE MODEL_NAME ='ACSAT')
+
+ IF NOT EXISTS (SELECT 1 FROM CSS_QUESTION_MASTER WHERE MODEL_ID = @MODELID)   
+ BEGIN
+
+INSERT INTO CSS_QUESTION_MASTER( MODEL_ID
+,QUESTION_CATEGORY
+,QUESTION
+,EFFECTIVE_FROM
+,CREATED_BY
+,CREATED_DATE
+,UPDATED_BY
+,UPDATED_DATE
+,ISACTIVE
+,QUESTION_DETAIL
+,RATING_SCALE
+,RATING_PARAM
+,PARAM_CATEGORY
+,TRIGGER_RCA
+,[SEQUENCE]
+,PERSPECTIVE)
+
+VALUES (@MODELID, 'NPS'	,'How likely are you to recommend Neurealm (Formerly GS Lab | GAVS) to a friend or colleague, if asked for your advice?', GETDATE(),'104744',GETDATE(),'104744',GETDATE(),	1,NULL,2,'Net Promoter Score',NULL,1,1,'Net Promoter Score'),
+(@MODELID, 'Criteria'	,'How satisfied are you with Neurealm in terms of meeting their commitment on agreed deliverables?',GETDATE(),'104744',GETDATE(),'104744',GETDATE(),1,NULL,2,'Delivery',NULL,1,2,'Delivery'),
+(@MODELID, 'Criteria'	,'How satisfied are you with Neurealm Team''s engagement and relationship with you and your team?',	GETDATE(),'104744',GETDATE(),'104744',GETDATE(),1,NULL,2,'Customer Relationship',NULL,1,3,'Customer Relationship'),
+(@MODELID, 'Criteria'	,'How satisfied are you with Neurealm as a Partner adding value to your business?',	GETDATE(),'104744',GETDATE(),'104744',GETDATE(),1,NULL,2,'Partnership',NULL,1,4,'Partnership'),
+(@MODELID, 'Others'	    ,'Please list your top expectations where Neurealm is doing well.', GETDATE(),'104744',GETDATE(),'104744',GETDATE(),1,NULL,3,NULL,NULL,1,5,NULL),
+(@MODELID, 'Others'	    ,'Please list your top expectations where Neurealm can do better.',	GETDATE(),'104744',GETDATE(),'104744',GETDATE(),1,NULL,2,NULL,NULL,1,6,NULL)
+
+END
+
+GO
