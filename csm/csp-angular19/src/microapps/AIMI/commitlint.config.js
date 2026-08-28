0ee072c652721cde62cfd8fab8dd006faabe3a84
Srividhya B <srividhya.b@neurealm.com>
2026-08-28 21:16:59 +0530

    AIMI - SAST - Medium Fix

--- csm/csp-angular19/src/microapps/AIMI/commitlint.config.js
+++ csm/csp-angular19/src/microapps/AIMI/commitlint.config.js
@@ -32,7 +32,7 @@ export default {
   },
   parserPreset: {
     parserOpts: {
-      headerPattern: /^([A-Z]+):\s*\(([a-z.-]+(?:[ \t][a-z.-]+)*)\)\s*-\s*(.+)$/,
+      headerPattern: /^([A-Z]+): \(([a-z0-9 .-]+)\) - (.+)$/,
       headerCorrespondence: ['type', 'scope', 'subject'],
     },
   },
